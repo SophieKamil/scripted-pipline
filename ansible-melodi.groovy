7c20ea6196099c899b9be22e839044121b726dd7
SophieKamil <sophie.s.kamil@gmail.com>
2020-08-28 17:30:24 +0000

    version 1

---
+++ ansible-melodi.groovy
@@ -0,0 +1,20 @@
+parameters([
+        string(defaultValue: '', description: 'Please enter VM IP', name: 'nodeIP', trim: true),
+        string(defaultValue: '', description: 'Please enter branch name', name: 'branch', trim: true)
+        ])
+    ])
+if (nodeIP?.trim()) {
+    node {
+        withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-master-ssh-key1', keyFileVariable: 'SSHKEY', passphraseVariable: '', usernameVariable: 'SSHUSERNAME')]) {
+            stage('Pull Repo') {
+                git changelog: false, poll: false, url: 'https://github.com/ikambarov/ansible-melodi.git'
+            }
+            stage("Run ansible-playbook") {
+                sh 'ansible-playbook -i "161.35.120.248,"  --private-key jenkins-master_rsa main.yml'
+            }
+        }
+    }
+}
+else {
+    error 'Please enter valid IP address'
+}
