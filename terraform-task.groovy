98b49d0877a0e0023a1d966010064f5df07cdbed
SophieKamil <sophie.s.kamil@gmail.com>
2020-09-07 03:27:43 +0000

    added Terraform Install

--- terraform-task.groovy
+++ terraform-task.groovy
@@ -1,31 +1,21 @@
-properties([
-    parameters([
-        string(defaultValue: '', description: 'Please provide remote IP to provision.', name: 'remoHost', trim: true)
-        ])
-    ])
-if (remoHost.length() > 6) {
-        node {
-            stage('Pull Repo') {
-                git url: 'https://github.com/SophieKamil/ansible-petclinic.git'
-            }
-            withEnv(['ANSIBLE_HOST_KEY_CHECKING=False', 'SPRINGPETCLINIC_REPO=https://github.com/ikambarov/spring-petclinic.git', 'SPRINGPETCLINIC_BRANCH=master']) {
-                stage("Install Prerequisites"){
-                    ansiblePlaybook credentialsId: 'jenkins-master-ssh-key1', inventory: "${params.remoHost},", playbook: "${WORKSPACE}/prerequisites.yml"
-                    }
-                stage("Pull SpringPetClinic"){
-                    ansiblePlaybook credentialsId: 'jenkins-master-ssh-key1', inventory: "${params.remoHost},", playbook: "${WORKSPACE}/pull_repo.yml"
-                    }
-                stage("Install java"){
-                    ansiblePlaybook credentialsId: 'jenkins-master-ssh-key1', inventory: "${params.remoHost},", playbook: "${WORKSPACE}/install_java.yml"
-                    }
-                stage("Install maven"){
-                    ansiblePlaybook credentialsId: 'jenkins-master-ssh-key1', inventory: "${params.remoHost},", playbook: "${WORKSPACE}/install_maven.yml"
-                    }
-                stage("Start SpringPetClinic"){
-                    ansiblePlaybook credentialsId: 'jenkins-master-ssh-key1', inventory: "${params.remoHost},", playbook: "${WORKSPACE}/start_app.yml"
-                    }
-            }  
+node{
+    stage("Pull Repo"){
+       git branch: 'solution', url: 'https://github.com/ikambarov/terraform-task.git'
+    }
+
+    withCredentials([usernamePassword(credentialsId: 'jenkins-aws-access-key1', passwordVariable: 'AWS_SECRET_ACCESS_KEY', usernameVariable: 'AWS_ACCESS_KEY_ID')]) {
+        stage("Terrraform Init"){
+            sh '''
+                cd sandbox/
+                terraform-0.13 version
+                terraform-0.13 init
+            '''
         }
-    } else {
-        error 'Please enter valid IP address for remote host'
+        stage("Terraform Apply"){
+            sh '''
+                cd sandbox/
+                terraform-0.13 apply -auto-approve
+            '''
+        }
+    }    
 }
